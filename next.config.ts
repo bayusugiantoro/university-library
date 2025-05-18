8bcda002856b710fc750b7624581d9785e8b3ead
Bayu Sugiantoro <bayusugiantoro5@gmail.com>
2025-05-18 15:22:58 +0700

    home data

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+        port: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
